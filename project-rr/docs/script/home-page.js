11a2579fd3f7d6cde224f809557d80d0660f2095
Ryan Reeves <reeves0ryan@gmail.com>
2022-05-17 20:54:53 -0700

    localstorage code ammended and condensed

--- project-rr/docs/script/home-page.js
+++ project-rr/docs/script/home-page.js
@@ -1,8 +1,7 @@
 var ingredientList = document.querySelector('#inputIngredientsList');
 var generate = document.querySelector('#generateButton');
 var ulItems = document.querySelector('#ulItems');
-userIngredientListItems = [];
-
+var loStoKeySelect = 0;
 
 generate.addEventListener('click', enterButton);
 
@@ -10,37 +9,22 @@ generate.addEventListener('click', enterButton);
 function enterButton() {
     var userInput = document.querySelector('input').value;
     
-        if(document.getElementById('inputIngredientsList').value.length == 0) {
-            alert('blank');
+    if(document.getElementById('inputIngredientsList').value.length == 0) {
+        alert('blank');
 
-        }else {
-            userIngredientListItems.push(userInput);
-            localStorage.setItem("userIngredientListItems", JSON.stringify(userInput))
-            renderMessage();
-        }
-}
+    }else {
+        var listedItems = document.querySelector('#display-items');
 
-function renderMessage() {
-    var copyPaste = JSON.parse(localStorage.getItem("userIngredientListItems"));
-    var listedItems = document.querySelector('#display-items');
+        var itemsEntered = document.createElement('div');
+        var enterText = document.createElement('h3');
+        itemsEntered.setAttribute("id", "listed-ing");
+    
+        enterText.textContent = userInput;
+    
+        listedItems.append(itemsEntered);
+        itemsEntered.append(enterText);
 
-    if(copyPaste !== null) {
-        var x = 0;
-        
-        createDivInput(userIngredientListItems[x]);
-
-        function createDivInput (food) {
-            var itemsEntered = document.createElement('div');
-            var enterText = document.createElement('h3');
-
-            itemsEntered.setAttribute("id", "listed-ing");
-
-            enterText.textContent = userIngredientListItems.slice(-1);
-            console.log(userIngredientListItems.slice(-1));
-
-            listedItems.append(itemsEntered);
-            itemsEntered.append(enterText);
-        }
-        x++;
+        localStorage.setItem(loStoKeySelect, JSON.stringify(userInput));
+        loStoKeySelect++;
     }
 }
