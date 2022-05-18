c6ff5040e26126663977fb17a384fe53e27a26fe
Eyesack <isaac.laflamme@gmail.com>
2022-05-18 16:40:58 -0700

    updated links oopsie

--- project-IL/Main/assets/js/home-page.js
+++ project-IL/Main/assets/js/home-page.js
@@ -11,7 +11,7 @@ generate.addEventListener('click', getIng);
 
 
 function sendIng(allIngArr) {
-    var queryString = '../recipe-page/recipe-page.html?ing='
+    var queryString = '../main/recipe-page.html?ing='
 
     for (i = 0; i < allIngArr.length; i++) {
         if (i == (allIngArr.length)-1){
