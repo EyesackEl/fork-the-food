6c84cb37441d1d589671b990274ee90e8ef76376
Ryan Reeves <reeves0ryan@gmail.com>
2022-05-18 22:05:12 -0700

    added JS and CSS for favorites page, finished 99% of CSS for recipe page, changed some HTML ids for continuity across all pages

---
+++ project-rr/docs/script/recipe-page.js
@@ -0,0 +1,4 @@
+var favElem = document.querySelector('.fav-button-text');
+favElem.addEventListener('click', function (event) {
+    document.documentElement.style.setProperty('--favtext', 'asdasdasda')
+});
